f8d3a851df6c27e90deff3af969128b62f987e8d
Hamza <jhamzaahmed@gmail.com>
2024-03-01 20:47:12 +0500

    started working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,22 @@ export const Navbar = () => {
   return (
     <header>
       <div className="container header-wrapper">
-        <div className="logo-sec"></div>
+        <div className="logo-sec">
+          <p>Logo</p>
+        </div>
         <nav>
           <ul>
             <li>
               <a href="#">Home</a>
             </li>
             <li>
-              <a href="#">Home</a>
+              <a href="#">About</a>
             </li>
             <li>
-              <a href="#">Home</a>
+              <a href="#">Projects</a>
             </li>
             <li>
-              <a href="#">Home</a>
+              <a href="#">Contact</a>
             </li>
           </ul>
         </nav>
